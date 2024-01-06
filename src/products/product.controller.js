6bff8aeda61aa64613128c21004d29fc551db4b7
Anil Kumar Vind <akumar64587@gmail.com>
2024-01-06 17:01:41 +0530

    add product more futures

--- src/products/product.controller.js
+++ src/products/product.controller.js
@@ -1,16 +1,94 @@
-const Product = require('../products/product.module');
+const Product = require("../products/product.module");
 
+module.exports.addproduct = async (req, res) => {
+  const {
+    name,
+    description,
+    price,
+    category,
+    brand,
+    stock,
+    imageUrl,
+    createdAt,
+    updatedAt,
+  } = req.body;
 
-module.exports.addproduct = async(req, res)=>{
-    const {name,  description, price, category, brand, stock, imageUrl, createdAt, updatedAt}=req.body;
+  try {
+    const product = new Product({
+      name,
+      description,
+      price,
+      category,
+      brand,
+      stock,
+      imageUrl,
+      createdAt,
+      updatedAt,
+    });
+    const newproduct = await product.save();
+    res.status(200).json({ message: "product add secusssfully", newproduct });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: "sever error" });
+  }
+};
 
+// Update an existing product
+
+module.exports.update = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      return res.status(404).json({ error: "product is not Available" });
+    }
+    product.set(req.body);
+    const newproduct = await product.save();
+    res.json(newproduct);
+  } catch (err) {
+    res.status(500).json("Error" + err);
+  }
+};
+
+// Delete a product
+
+module.exports.delete = async(req, res)=>{
     try {
-        const product = new Product({name,  description, price, category, brand, stock, imageUrl, createdAt, updatedAt});
-        const newproduct = await product.save();
-        res.status(200).json({message:"product add secusssfully", newproduct});
-
+        const product = await Product.findByIdAndDelete(req.params.id);
+        if(!product){
+            res.status(401).json({message:"product is Available"})
+        }
+        res.status(200).json({message:"product Deleted", product});
     } catch (error) {
         console.log(error);
         res.status(500).json({error:"sever error"});
+    }
+}
+
+
+// View details of a specific product
+
+
+module.exports.getproduct = async(req, res)=>{
+    try {
+        const product = await Product.findById(req.params.id);
+        if(!product){
+          return  res.status(404).json({message:"product is not Available"});
+        }
+        res.status(200).json({message:"View product", product});
+    } catch (error) {
+        console.log('error');
+        res.status(500).json({error:"sever error"});
+    }
+}
+
+
+
+module.exports.getallproduct = async(req, res)=>{
+    try {
+        const product = await Product.find();
+        res.status(200).json({message:"view all product", product});
+    } catch (error) {
+       console.log(error);
+       res.status(500).json({error:"sever error"}); 
     }
 }
--- src/products/product.router.js
+++ src/products/product.router.js
@@ -3,6 +3,9 @@ const router = express.Router();
 const productcontroller = require('../products/product.controller');
 
 router.post('/addproduct', productcontroller.addproduct);
-
+router.put('/update/:id', productcontroller.update);
+router.delete('/delete/:id', productcontroller.delete);
+router.get('/getallproduct', productcontroller.getallproduct);
+router.get('/getproduct/:id', productcontroller.getproduct);
 
 module.exports = router;
